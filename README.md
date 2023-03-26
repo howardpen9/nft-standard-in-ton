7bbc5100e52c3f760645d1cbc4617421c3fbf9a9
Howard Peng <howard.pen9@gmail.com>
2023-03-27 00:03:56 +0800

    update
    d

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# nft-standard-in-ton
+# TACT NFT standard implement
+
+This is the project that can help you easily deploy an NFT collection on TON blockchain! (\*In test-net)
+
+Remember to go to `depoly.ts` to change the parameters you want!
+
+```bash
+yarn build # To build contract
+yarn deploy # To deploy contract
+yarn test # To test contract..?
+```
+
+## Verify your NFT
+
+-   https://testnet.explorer.tonnft.tools/
+
+## Overview
+
+[Tact documentation.](https://github.com/ton-community/tact/blob/main/docs/overview.md)
+[Tact Dev TG group](https://t.me/tactlang)
+
+## Licence
+
+MIT
---
+++ sources/contract.deploy.ts
@@ -0,0 +1,38 @@
+import { beginCell, contractAddress, toNano, Cell, Address } from "ton";
+import { testAddress } from "ton-emulator";
+import { deploy } from "./utils/deploy";
+import { printAddress, printDeploy, printHeader } from "./utils/print";
+// ================================================================= //
+import { NftCollection } from "./output/sample_NftCollection";
+// ================================================================= //
+(async () => {
+    const OFFCHAIN_CONTENT_PREFIX = 0x01;
+    const string_first = "https://s.getgems.io/nft-staging/c/628f6ab8077060a7a8d52d63/";
+    let newContent = beginCell().storeInt(OFFCHAIN_CONTENT_PREFIX, 8).storeStringRefTail(string_first).endCell();
+
+    let body = beginCell().storeUint(0, 32).storeStringTail("Mint").endCell();
+
+    // Parameters
+    let owner = Address.parse(""); // Replace owner with your address
+    let init = await NftCollection.init(
+        owner, 
+        newContent, 
+        {
+            $$type: "RoyaltyParams",
+            numerator: 350n, // 350n = 35%
+            denominator: 1000n,
+            destination: owner,
+        }
+    );
+
+    let address = contractAddress(0, init);
+    let deployAmount = toNano("0.5");
+    let testnet = true;
+
+    printHeader("sampleNFT_Contract");
+    printAddress(address);
+    // printDeploy(init, deployAmount, packed, testnet);
+
+    // Do deploy
+    await deploy(init, deployAmount, body, testnet);
+})();
